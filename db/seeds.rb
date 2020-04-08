d623ff6653c2af51121c11bb08f112295bd4ba7f
ksdaishu <ksdaishu@gmail.com>
2020-04-08 16:27:43 +0530

    Pouplate seed file with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |x|
+	Blog.create!(
+    	title:"My Blog post #{x}",
+    	body:'Flowers are also called the bloom or blossom of a plant. Flowers have petals. Inside the part of the flower that has petals are the parts which produce pollen and seeds. In all plants, a flower is usually its most colourful part.'
+	)
+end	
+
+puts "10 bloges were created successfully"
+
+5.times do |s|
+	Skill.create!(
+		title:"Ruby on Rails #{s}",
+		percent:75
+		)
+end
+
+puts "5 skills were created successfully"
+
+
+9.times do |p|
+	Portfolio.create!(
+		title:"Portfolio tilte:#{p}",
+		subtitle:'My great service',
+        body:'Flowers are also called the bloom or blossom of a plant. Flowers have petals. Inside the part of the flower that has petals are the parts which produce pollen and seeds. In all plants, a flower is usually its most colourful part.',
+        main_image:'https://placehold.it/600x400',
+        thumb_image:'https://placehold.it/350x200'
+		)
+end	
+puts "9 portfolio items created suucessfully"	
