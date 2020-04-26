c2daaf3ac5a4c056019ae54f8dd78f70cbbc5f12
ksdaishu <ksdaishu@gmail.com>
2020-04-26 21:15:53 +0530

    backend for added  to accept multiple value

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
-	has_many :Technologies
+	has_many :technologies
+	accepts_nested_attributes_for :technologies, reject_if: lambda {|attrs| attrs['name'].blank?}
 	include Placeholder
 	validates :title, presence: true
 	validates :body, presence: 
