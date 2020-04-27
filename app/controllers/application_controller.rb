7f11be6742bb268d70130a926676e352f813d2d2
ksdaishu <ksdaishu@gmail.com>
2020-04-27 22:27:52 +0530

    Session tracker added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
 	include DeviseWhitelist
+
+	before_action	:setsource
+
+	def setsource
+		session[:source] = params[:q] if params[:q]
+	end
 end
